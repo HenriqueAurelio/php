19ae2e8b775a00ad368430cfc8ffa91aefa4d375
Henrique <henriqueaureliocsilva@gmail.com>
2020-06-08 12:24:30 -0300

    novo

---
+++ app/views/partials/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aurelio</title>
+</head>
+<body>
+    <?php require('nav.php'); ?>
--- views/partials/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/sobrenos">Sobre nos</a></li>
-        <li><a href="/contato">Contato</a></li>
-    </ul>
-</nav>
